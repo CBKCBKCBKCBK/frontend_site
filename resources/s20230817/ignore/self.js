3619cd993c514ff0369c71dac43aa0c553bba2dc
CBK <cbkcbkcbkcbk@yahoo.com.tw>
2023-08-17 16:34:12 +0800

    basket要修正最高分部分 把圖表補上並全部完工

---
+++ resources/s20230817/ignore/self.js
@@ -0,0 +1,107 @@
+function auth_url(){
+    return "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization="+mykey()+"&format=JSON&locationName="
+}
+/**
+ * 
+ * @returns 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+function mykey(){
+    return "CWB-1A9F9190-EA6D-4F72-8CC4-806EB7594209"
+}
